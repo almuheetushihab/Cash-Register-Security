2d625e94480afa195b008fe0756271e588843e7f
Md. AL MUHEETU <almuheetu@gmail.com>
2026-03-31 16:00:36 +0600

    Replace sample UI with RegisterScreen
    Swap out the placeholder Greeting composable and theme scaffolding for the app's RegisterScreen as the entry UI. Remove enableEdgeToEdge call and sample Greeting/Preview functions, simplify imports, and wrap the content in MaterialTheme/Surface. Add @RequiresApi(Build.VERSION_CODES.O) to onCreate to reflect API requirements.

--- app/src/main/java/com/shihab/cashregistersecurity/MainActivity.kt
+++ app/src/main/java/com/shihab/cashregistersecurity/MainActivity.kt
@@ -1,47 +1,24 @@
 package com.shihab.cashregistersecurity
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.shihab.cashregistersecurity.ui.theme.CashRegisterSecurityTheme
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.shihab.cashregistersecurity.ui.screens.RegisterScreen
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            CashRegisterSecurityTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface {
+                    RegisterScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CashRegisterSecurityTheme {
-        Greeting("Android")
     }
 }
